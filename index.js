0a746f7f05f26e11c23b0cb1316b13791b2c49a4
Jeff Lam <jefflam1200@gmail.com>
2016-06-03 12:00:40 -0700

    Updated to store values in chrome so it is retrievable from any tab

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var box = document.createElement('div');
-box.textContent = "You've been using the computer for 30 minutes - Please take a 2 minute walk to rest your eyes."; 
-box.setAttribute('class','popup');
 
-var i = 1800;
+function saveChanges() {
+    var box = document.createElement('div');
+    box.textContent = "You've been using the computer for 30 minutes - Please take a 2 minute walk to rest your eyes.";
+    box.setAttribute('class', 'popup');
 
-var intervalID = setInterval(function(){
-if(i === 0){
-document.body.appendChild(box);
+    var i = 5;
+
+    var intervalID = setInterval(function() {
+        if (i === 0) {
+            document.body.appendChild(box);
+        }
+        chrome.storage.sync.set({
+            'value': i
+        }, function() {
+            console.log(i);
+        });
+        i--;
+    }, 1000);
+    chrome.storage.sync.get('value', function(obj) {
+        console.log(obj);
+    });
+
 }
-i--; 
-},1000); 
-
-
-
-
